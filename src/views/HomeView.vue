e64ad5fcc95dc999dcbd0b918111006e7189096e
Juan Garcia <garcia.juanv97@gmail.com>
2023-03-30 14:17:29 -0300

    Changes state button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -744,8 +744,10 @@ img {
             <td class="content-table" v-else-if="item.state == 'cargado'"></td>
             <td class="content-table" v-else-if="item.state == 'eliminado'">Retirar</td>
 
-            <td class="content-table"  v-if="item.state != 'cargado' || item.state == 'eliminado'"> <button v-bind:id="item.class_sale" :disabled="item.button ? false : ''" v-on:click="update_status_order($event)" class="status inactive ">Cargar</button> </td>
-            <td class="content-table"  v-else><span class="status active">Listo</span></td>
+            <td class="content-table" v-if="item.state == 'no_cargado' || item.state == 'preparar_retiro' || item.state == 'envio' || item.state == 'send_chilexpress' || item.state == 'programado'"> <button v-bind:id="item.class_sale" :disabled="item.button ? false : ''" v-on:click="update_status_order($event)" class="status inactive ">Cargar</button> </td>
+            <td class="content-table" v-else-if="item.state == 'cambio_tienda' || item.state == 'retirar'"> <button v-bind:id="item.class_sale" :disabled="item.button ? false : ''" v-on:click="update_status_order($event)" class="status inactive ">Retirar</button> </td>
+            <td class="content-table" v-else><span class="status active">Listo</span></td>
+
           </tr>
           <template v-for="(item2, index2) in item.productos.slice(1)">
             <tr :key="index2" v-if="`${item.id} ${item2.product_model} ${item.productos[0].product_id} ${item.state} ${item.action} ${item.cliente}`.search(new RegExp(search, 'i')) >= 0 || search == ''">
